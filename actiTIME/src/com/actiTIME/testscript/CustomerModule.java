525824630c4c2b08b6ac91054204e2ff199430fc
ASUS <ASUS@DESKTOP-CFPAVKO>
2023-02-08 19:29:06 +0530

    modifyCustomer

--- actiTIME/src/com/actiTIME/testscript/CustomerModule.java
+++ actiTIME/src/com/actiTIME/testscript/CustomerModule.java
@@ -1,5 +1,18 @@
 package com.actiTIME.testscript;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class CustomerModule {
+	
+	@Test
+	public void testCreateCustomer() {
+		
+	}
+	
+	public void testModifyCustomer() {
+		
+		Reporter.log("modify");
+	}
 
 }
